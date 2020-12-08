9abc488caa4d85979072a054ec2c72b88a21e88f
Yeasin Abedin <yabe@stormgeo.com>
2020-12-08 20:55:07 +0600

    Initial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      {/* Home component */}
+    </div>
+  );
+}
+
+export default App;
