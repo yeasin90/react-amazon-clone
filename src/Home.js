7867a4c9743d6c7934c85059c664581f48b494e4
Yeasin Abedin <yabe@stormgeo.com>
2020-12-09 00:01:21 +0600

    04-products-with-properties - Display products with properties

--- src/Home.js
+++ src/Home.js
@@ -13,18 +13,48 @@ function Home() {
         />
 
         <div className='home__row'>
-          <Product />
-          <Product />
+          <Product
+            title='The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback'
+            price={11.96}
+            image='https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg'
+            rating={5}
+          />
+          <Product
+            title='Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl'
+            price={239}
+            image='https://images-na.ssl-images-amazon.com/images/I/81O%2BGNdkzKL._AC_SX450_.jpg'
+            rating={4}
+          />
         </div>
 
         <div className='home__row'>
-          <Product />
-          <Product />
-          <Product />
+          <Product
+            title='Samsung LC49RG90SSUXEN 49 Curved LED Gaming Monitor'
+            price={199.99}
+            image='https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg'
+            rating={3}
+          />
+          <Product
+            title='Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric'
+            price={98.99}
+            image='https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$'
+            rating={5}
+          />
+          <Product
+            title='New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation)'
+            price={598.99}
+            image='https://images-na.ssl-images-amazon.com/images/I/816ctt5WV5L._AC_SX385_.jpg'
+            rating={4}
+          />
         </div>
 
         <div className='home__row'>
-          <Product />
+          <Product
+            title='Samsung LC49RG90SSUXEN 49 Curved LED Gaming Monitor - Super Ultra Wide Dual WQHD 5120 x 1440'
+            price={1094.98}
+            image='https://images-na.ssl-images-amazon.com/images/I/6125mFrzr6L._AC_SX355_.jpg'
+            rating={4}
+          />
         </div>
       </div>
     </div>
--- src/Product.js
+++ src/Product.js
@@ -1,21 +1,24 @@
 import React from 'react'
 import './Product.css'
 
-function Product() {
+function Product({ title, image, price, rating }) {
   return (
     <div className='product'>
       <div className='product__info'></div>
-      <p>The lean starup</p>
+      <p>{title}</p>
       <p className='product__price'>
         <small>$</small>
-        <strong>19.99</strong>
+        <strong>{price}</strong>
       </p>
-      <div className='product__rating'></div>
+      <div className='product__rating'>
+        {Array(rating)
+          .fill()
+          .map((_, i) => (
+            <p>⭐</p>
+          ))}
+      </div>
 
-      <img
-        src='https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg'
-        alt
-      />
+      <img src={image} alt />
 
       <button>Add to Basket</button>
     </div>
